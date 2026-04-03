ad8ac1467ce5ad0cb62e436f504fc592a5d829f0
Nilesh Sahani <136577282+Sahanilesh@users.noreply.github.com>
2026-04-03 16:02:08 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ connectDB();
 app.use(cors({
     origin: [
     "http://localhost:5173",
-    "https://your-frontend.vercel.app"
+    "https://interview-iq-vert.vercel.app/"
   ],
     credentials: true
 }))
